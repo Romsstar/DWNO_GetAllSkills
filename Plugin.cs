2f9b2407bab2703c0b2eac1506edee0ac7cfc136
Romsstar <romsstar@gmail.com>
2024-06-30 01:47:00 +0200

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,45 @@
+﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+
+
+namespace BackToTheTitleScreen;
+
+[BepInPlugin(GUID, PluginName, PluginVersion)]
+[BepInProcess("Digimon World Next Order.exe")]
+public class Plugin : BasePlugin
+{
+    internal const string GUID = "Romsstar.DWNO.GetAllSkills";
+    internal const string PluginName = "GetAllSkills";
+    internal const string PluginVersion = "1.0";
+
+    public override void Load()
+    {
+        Awake();
+    }
+
+    public void Awake()
+    {
+        Harmony harmony = new Harmony("Patches");
+        harmony.PatchAll();
+    }
+
+    [HarmonyPatch(typeof(PlayerData))]
+    [HarmonyPatch("ReadSaveData")]
+    public static class MainTitlePatch
+    {
+        private static bool flagsSet = false;
+
+        [HarmonyPostfix]
+        public static void ReadSaveData_Postfix(MainTitle __instance)
+        {
+            if (!flagsSet && StorageData.m_playerData != null && StorageData.m_playerData.m_AttacSkillkFlag != null)
+            {
+                for (uint i = 0; i < StorageData.m_playerData.m_AttacSkillkFlag.GetNumFlags(); i++)
+                {
+                    StorageData.m_playerData.m_AttacSkillkFlag[i] = true;
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# DWNO Get All Skills Mod
+This mod unlocks all Digimon Skills right away.
+This is useful for testing purposes or if you just want to have all skills available from the start.
+
+# Installation
+1. Install BepInEx (Tutorial by Yasha-jin)
+2. Download the latest release from the Releases Tab
+3. Copy the GetAllSkills.dll into the plugins folder of BepinEx
+4. Start the game and enjoy :)
+
+# Contact
+Discord: Romsstar#8098, or in either the Digimon Modding Community or Digimon Discord Community
